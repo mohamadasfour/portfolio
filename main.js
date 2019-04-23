1fbfd817a4a7d1812e37da6f4d273347f3034ea2
MohamadAsfour <mohamad.asfour.0001@gmail.com>
2019-04-23 08:06:28 +0300

    add function call

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-let element = document.getElementById("target-id");
-document.addEventListener("scroll", function() { 
-    var scrollAmt = window.pageYOffset || document.documentElement.scrollTop
-    if(window.innerHeight/2 >= scrollAmt)
-    element.style.visibility = "hidden";
-    else
-    element.style.visibility = "visible";
-});
-// window.onscroll = () => {
-//     const Ypos = window.pageYOffset || document.documentElement.scrollTop;
-    
-//     if(Ypos < 100) document.getElementById("target-id").style.visibility = "hidden";
+// function testScroll(ev){
+//     if(window.pageYOffset>100)alert('User has scrolled at least 400 px!');
+// }
+// window.onscroll=testScroll
+
+// function scrollFunction() {
+//     document.getElementById("target-id").style.visibility = "show";   //To hide the element.
+// }
+
+// window.onscroll = scrollFunction;
+// window.scroll = () => {
+
+//     const Ypos = window.pageYOffset;
+  
+//     if(Ypos < 150) console.log('hiddin'),document.getElementById("target-id").style.visibility = "hidden";
    
-//    else if (Ypos > 150) document.getElementById("target-id").style.visibility = "visible";
-    
-//   }, false;
+//     else{
+//         document.getElementById("target-id").style.visibility = "visible";
+//     }
+  
+//   }
+//   scroll();
+window.onscroll = () => {
+
+    const Ypos = window.pageYOffset;
+  
+    if(Ypos < 200) document.getElementById("target-id").style.visibility = "hidden";
+   
+    else{
+        document.getElementById("target-id").style.visibility = "visible";
+    }
+  
+  }
+  onscroll();
+
